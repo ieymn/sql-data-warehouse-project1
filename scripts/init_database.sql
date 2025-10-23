70e1fb8a997df43c9302e9495baa414fcc82ae47
ieymn <ieymnn17@gmail.com>
2025-10-23 18:39:03 +0800

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,42 @@
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'IntegratedSalesDW' after checking if it already exists. 
+    If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
+    within the database: 'bronze', 'silver', and 'gold'.
+	
+WARNING:
+    Running this script will drop the entire 'IntegratedSalesDW' database if it exists. 
+    All data in the database will be permanently deleted. Proceed with caution 
+    and ensure you have proper backups before running this script.
+*/
+
+USE master;
+GO
+
+-- Drop and recreate the 'IntegratedSalesDW' database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'IntegratedSalesDW')
+BEGIN
+    ALTER DATABASE IntegratedSalesDW SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE IntegratedSalesDW;
+END;
+GO
+
+-- Create the datawarehouse database as 'IntegratedSalesDW'
+CREATE DATABASE IntegratedSalesDW;
+GO
+
+USE IntegratedSalesDW;
+GO
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
+CREATE SCHEMA gold;
+GO
